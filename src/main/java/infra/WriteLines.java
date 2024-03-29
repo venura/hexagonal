a06353815d9c8505a8b83eff25b7cd35061e5abe
Chanaka <venuraalwis@gmail.com>
2024-03-29 18:42:17 +0530

    Update WriteLines.java

--- src/main/java/infra/WriteLines.java
+++ src/main/java/infra/WriteLines.java
@@ -1,5 +1,6 @@
 package infra;
 
+//This comes under infra because this is a trick to make ports and adapter testable therefore its not going under ports
 public interface WriteLines {
     void writeLine(String verses);
 }
